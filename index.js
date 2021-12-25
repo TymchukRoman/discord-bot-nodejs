3f4e018d6e8b34e2dc76ad67853264124b720d06
romantymchuk <roman.t@aquimarketing.com>
2021-12-25 21:10:35 +0200

    Database connected

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const Discord = require("discord.js");
+const mongoose = require("mongoose");
+require('dotenv').config();
 const config = require("./config.json");
 const commands = require("./commands/commands_router");
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 const prefix = "!";
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on("error", err => {
+    console.log("Error: ", err)
+})
+mongoose.connection.on("connected", (err, res) => {
+    console.log("Mongoose is connected")
+})
 
 client.on("messageCreate", function (message) {
 
